7bfe3577c4c4578d6c72da18b854b4ef27dc7287
InsaneHe <134916618+InsaneHe@users.noreply.github.com>
2024-10-27 20:36:45 +0800

    Update BSM_Model.py

--- BSM_Model.py
+++ BSM_Model.py
@@ -1,73 +1,37 @@
+from scipy.stats import norm
 import numpy as np
-import scipy.integrate as spi
 
+# 计算公式参考：https://en.wikipedia.org/wiki/Black%E2%80%93Scholes_model
 
-class Heston_Model:
-    def __init__(self, K, t, S0, r, v0, theta, kappa, sigma, rho):
-        self.K = K
-        self.t = t
-        self.S0 = S0
-        self.r = r
-        self.v0 = v0
-        self.theta = theta
-        self.kappa = kappa
-        self.sigma = sigma
-        self.rho = rho
+# 计算BSM模型的期权价格
+class BSM_Model:
 
-    def characteristic_function(self, phi, type):
-        if type == 1:
-            u = 0.5
-            b = self.kappa - self.rho * self.sigma
+    def __init__(self, S, K, T, r, sigma):
+        self.S = S  # 当前股票价格
+        self.K = K  # 行权价
+        self.T = T  # 到期时间
+        self.r = r  # 无风险利率
+        self.sigma = sigma  # 波动率，该数据通常由历史数据计算得到，一般取20%左右
+        self.option_type = "call"# 期权类型
+
+    # d1: (ln(S/K) + (r + 0.5*sigma^2)*T) / (sigma*sqrt(T))
+    def d1(self):
+        return (np.log(self.S / self.K) + (self.r + 0.5 * self.sigma**2) * self.T) / (
+            self.sigma * np.sqrt(self.T)
+        )
+
+    # d2: d1 - sigma*sqrt(T)
+    def d2(self):
+        return self.d1() - self.sigma * np.sqrt(self.T)
+
+    def price(self):
+        # 看涨：value= S*N(d1) - K*e^(-rT)*N(d2)
+        if self.option_type == "call":
+            return self.S * norm.cdf(self.d1()) - self.K * np.exp(
+                -self.r * self.T
+            ) * norm.cdf(self.d2())
+        # 看跌：value= K*e^(-rT)*N(-d2) - S*N(-d1)
         else:
-            u = -0.5
-            b = self.kappa
-
-        a = self.kappa * self.theta
-        x = np.log(self.S0)
-        d = np.sqrt(
-            (self.rho * self.sigma * phi * 1j - b) ** 2
-            - self.sigma**2 * (2 * u * phi * 1j - phi**2)
-        )
-        g = (b - self.rho * self.sigma * phi * 1j + d) / (
-            b - self.rho * self.sigma * phi * 1j - d
-        )
-        D = self.r * phi * 1j * self.t + (a / self.sigma**2) * (
-            (b - self.rho * self.sigma * phi * 1j + d) * self.t
-            - 2 * np.log((1 - g * np.exp(d * self.t)) / (1 - g))
-        )
-        E = (
-            ((b - self.rho * self.sigma * phi * 1j + d) / self.sigma**2)
-            * (1 - np.exp(d * self.t))
-            / (1 - g * np.exp(d * self.t))
-        )
-
-        return np.exp(D + E * self.v0 + 1j * phi * x)
-
-    def integral_function(self, phi, type):
-        integral = np.exp(
-            -1 * 1j * phi * np.log(self.K)
-        ) * self.characteristic_function(phi, type=type)
-        return integral
-
-    def P_Value(self, type):
-        """计算P值"""
-        ifun = lambda phi: np.real(self.integral_function(phi, type=type) / (1j * phi))
-
-        # 分段积分
-        intervals = [(0, 10), (10, 100), (100, 1000)]
-        result = 0
-        for interval in intervals:
-            res, err = spi.quad(ifun, interval[0], interval[1], limit=100)
-            result += res
-
-        return 0.5 + (1 / np.pi) * result
-
-    def Call_Value(self):
-        """计算看涨期权的价格"""
-        P1 = self.S0 * self.P_Value(type=1)
-        P2 = self.K * np.exp(-self.r * self.t) * self.P_Value(type=2)
-
-        if np.isnan(P1 - P2):
-            return 1000000
-        else:
-            return P1 - P2
+            return self.K * np.exp(-self.r * self.T) * norm.cdf(
+                -self.d2()
+            ) - self.S * norm.cdf(-self.d1())
